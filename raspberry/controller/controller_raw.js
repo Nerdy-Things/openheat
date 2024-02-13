d90b6a4f886dd24bd502d226ed94d798dff9c91b
Nerdy-Things <nerdythingschannel@gmail.com>
2024-02-12 19:51:07 -0700

    Fix raw page

--- raspberry/controller/controller_raw.js
+++ raspberry/controller/controller_raw.js
@@ -8,17 +8,23 @@ module.exports = {
         const startDate = core.util.datetime.formatToSQL(roundedTime.subtract(2, 'days'));
 
         const temperatures = await core.db.temperature.getTemperature(startDate, endDate, 10000);
-        const devices = {}
+        const temperatureGrouped = {}
+
+        const devices = await core.db.device.getAll();
+        const deviceMap = {}
+        for (device of devices) {
+            deviceMap[device['id']] = device
+        }
 
         temperatures.forEach(temperature => {
-            if (!devices[temperature['device_id']]) {
-                devices[temperature['device_id']] = []
+            if (!temperatureGrouped[temperature['device_id']]) {
+                temperatureGrouped[temperature['device_id']] = []
             }
             const date = moment(new Date(temperature.actual_time));
             const formattedDate = date.format("HH:mm:ss DD.MM.YYYY");
             temperature.time = formattedDate;
-            devices[temperature['device_id']].push(temperature);
+            temperatureGrouped[temperature['device_id']].push(temperature);
         });
-        core.renderer.render(req, res, "/raw/raw.html", {devices: devices});
+        core.renderer.render(req, res, "/raw/raw.html", {temperatureGrouped: temperatureGrouped, deviceMap: deviceMap});
     },
 }
